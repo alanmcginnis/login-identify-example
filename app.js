72305e096c668ce19a25bd29193b43a25bfb1d83
Alan J McGinnis <7727219+alanmcginnis@users.noreply.github.com>
2023-06-08 12:16:05 -0700

    Testing preview url fix

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
 const express = require('express');
-const mysql = require("mysql")
-// Required
-const dotenv = require('dotenv')
-dotenv.config({ path: './.env'})
-const path = require("path")
+// const mysql = require("mysql")
+// // Required
+// const dotenv = require('dotenv')
+// dotenv.config({ path: './.env'})
+// const path = require("path")
 
-// Database
-const db = mysql.createConnection({
-  host: process.env.DATABASE_HOST,
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE,
-  port: process.env.DATABASE_PORT
-})
+// // Database
+// const db = mysql.createConnection({
+//   host: process.env.DATABASE_HOST,
+//   user: process.env.DATABASE_USER,
+//   password: process.env.DATABASE_PASSWORD,
+//   database: process.env.DATABASE,
+//   port: process.env.DATABASE_PORT
+// })
 
-db.connect((error) => {
-  if(error) {
-      console.log(error)
-  } else {
-      console.log("MySQL connected!")
-  }
-})
+// db.connect((error) => {
+//   if(error) {
+//       console.log(error)
+//   } else {
+//       console.log("MySQL connected!")
+//   }
+// })
 
-// Config + Init
-const publicDir = path.join(__dirname, './public')
+// // Config + Init
+// const publicDir = path.join(__dirname, './public')
 const app = express();
 
-app.set('view engine', 'hbs')
-app.use(express.static(publicDir))
+// app.set('view engine', 'hbs')
+// app.use(express.static(publicDir))
 
 // Routes
 
