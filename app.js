2cdaf1933e7612f6dcaca905d2bb4ded2d2a5ef4
Alan J McGinnis <7727219+alanmcginnis@users.noreply.github.com>
2023-06-08 12:18:42 -0700

    Testing preview url fix

--- app.js
+++ app.js
@@ -36,6 +36,6 @@ app.get("/", (req, res) => {
   res.send('Hello World!');
 })
 
-app.listen(3000, ()=> {
+app.listen(process.env.PORT || 3000, ()=> {
   console.log("server started on port 3000")
 })
